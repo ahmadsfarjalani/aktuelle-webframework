4cf240b75870f405ac9666edc85ec9179331d376
Ahmad <s90358@bht-berlin.de>
2024-05-23 17:32:08 +0200

    First Commit

--- src/Human.ts
+++ src/Human.ts
@@ -15,3 +15,9 @@ const HumanSchema: Schema = new Schema({
 const Human = mongoose.model<IHuman>('Human', HumanSchema);
 
 export { Human, IHuman };
+
+const Test: Schema = new Schema({
+  name: { type: String, required: true },
+  password: { type: String, required: true },
+  alter: { type: Number, required: true },
+});
